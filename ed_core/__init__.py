88ba379727ce5c4bac63caadcbddff3b349f4d27
Vortex Acherontic <vortex@z-ray.de>
2022-12-28 16:39:59 +0100

    Load all backends beforehand

--- ed_core/__init__.py
+++ ed_core/__init__.py
@@ -4,10 +4,22 @@ from ed_core import streamdeck
 from ed_core.streamdeck_config import load_config, apply_config
 
 VERSION = "0.0.1"
+BACKENDS = {}
 
 
 def run():
     print("El Decko Core running.")
     load_config()
-    backends = entry_points(group='eldecko.backend')
-    streamdeck.initialize(backends)
+    discovered_backends = entry_points(group='eldecko.backend')
+    for edb in discovered_backends:
+        edb_id = edb.value.split(":")[0]
+        edb_function = edb.load()
+        edb_name = edb.name
+        if edb_id not in BACKENDS:
+            BACKENDS[edb_id] = []
+        BACKENDS[edb_id].append({
+            "name": edb_name,
+            "function": edb_function
+        })
+
+    streamdeck.initialize(discovered_backends)
