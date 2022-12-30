0df3cfaf4de814abab41f6c851833ac507379719
Vortex Acherontic <vortex@z-ray.de>
2022-12-30 17:42:46 +0100

    WIP unify dynamic data handling (later used by  backends)

---
+++ ed_core/dyn_data.py
@@ -0,0 +1,51 @@
+import os
+import json
+import shutil
+
+from xdg import (
+    xdg_cache_home,
+    xdg_config_dirs,
+    xdg_config_home,
+    xdg_data_dirs,
+    xdg_data_home,
+    xdg_runtime_dir,
+    xdg_state_home,
+)
+
+__app_name = "eldecko"
+CONFIG_ROOT: str = ""
+
+if os.name == "nt":
+    CONFIG_ROOT = os.getenv('LOCALAPPDATA') + "/" + __app_name + "/"
+else:
+    CONFIG_ROOT = str(xdg_config_home()) + "/" + __app_name + "/"
+
+CONFIG_ROOT_BACKEND = CONFIG_ROOT + "backends/"
+ASSETS_ROOT = CONFIG_ROOT + "assets/"
+
+
+def load_config(root_path: str, file_name: str):
+    if not os.path.exists(root_path):
+        os.makedirs(root_path)
+    try:
+        with open(root_path + file_name) as input_file:
+            data = json.load(input_file)
+            return data
+    except json.decoder.JSONDecodeError as e:
+        print(e)
+
+
+def store_config(root_path: str, file_name: str, data: dict):
+    if not os.path.exists(root_path):
+        os.makedirs(root_path)
+    with open(root_path + file_name, "w+", encoding="utf-8") as outfile:
+        json.dump(data, outfile, ensure_ascii=False, indent=2)
+
+
+def store_image(source: str):
+    file_name = os.path.basename(source).split('/')[-1]
+    shutil.copyfile(source, ASSETS_ROOT + file_name)
+
+
+def config_exists(root_path: str, file_name: str):
+    return os.path.isfile(root_path + file_name)
--- ed_core/streamdeck_config.py
+++ ed_core/streamdeck_config.py
@@ -3,32 +3,18 @@ import os
 
 from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.Devices.StreamDeck import StreamDeck
-from xdg import (
-    xdg_cache_home,
-    xdg_config_dirs,
-    xdg_config_home,
-    xdg_data_dirs,
-    xdg_data_home,
-    xdg_runtime_dir,
-    xdg_state_home,
-)
 
-CONFIG_PATH: str = str(xdg_config_home()) + "/eldecko/"
-CONFIG_FILE: str = CONFIG_PATH + "streamdeck.json"
+from ed_core import dyn_data
+
+CONFIG_FILE: str = "streamdeck.json"
 DECK_CFG: dict = {}
 
 
 def load_config():
-    if not os.path.exists(CONFIG_PATH):
-        os.makedirs(CONFIG_PATH)
-    if not os.path.isfile(CONFIG_FILE):
+    if not dyn_data.config_exists(dyn_data.CONFIG_ROOT, CONFIG_FILE):
         __create_default_config()
-    try:
-        with open(CONFIG_FILE) as input_file:
-            global DECK_CFG
-            DECK_CFG = json.load(input_file)
-    except json.decoder.JSONDecodeError as e:
-        print(e)
+    global DECK_CFG
+    DECK_CFG = dyn_data.load_config(dyn_data.CONFIG_ROOT, CONFIG_FILE)
 
 
 def get_config():
@@ -65,5 +51,4 @@ def __create_default_config():
 
         DECK_CFG[serial] = dict(brightness=30, key_config=key_config)
         deck.close()
-        with open(CONFIG_FILE, "w+", encoding="utf-8") as outfile:
-            json.dump(DECK_CFG, outfile, ensure_ascii=False, indent=2)
+        dyn_data.store_config(dyn_data.CONFIG_ROOT, CONFIG_FILE, DECK_CFG)
