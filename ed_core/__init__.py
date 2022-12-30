89bd1b9d48c648d930834a8307498c30a212ee79
Vortex Acherontic <vortex@z-ray.de>
2022-12-30 17:42:08 +0100

    Raised version to 2022.12.28.1 for pypi fix

--- ed_core/__init__.py
+++ ed_core/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import entry_points
 from ed_core import streamdeck
 from ed_core.streamdeck_config import load_config, apply_config
 
-VERSION = "2022.12.28"
+VERSION = "2022.12.28.1"
 BACKENDS = {}
 
 
