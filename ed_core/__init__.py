d1b69f72954245bf67fa073b17713acaba7efcdb
Vortex Acherontic <vortex@z-ray.de>
2023-05-06 23:06:37 +0200

    Add homepage and issue tracker for pypi

--- ed_core/__init__.py
+++ ed_core/__init__.py
@@ -3,7 +3,7 @@ from importlib.metadata import entry_points
 from ed_core import streamdeck
 from ed_core.streamdeck_config import load_config, apply_config
 
-VERSION = "2023.5.6"
+VERSION = "2023.5.6.1"
 BACKENDS = {}
 
 
