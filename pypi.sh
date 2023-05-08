d04ea31253435a4ffe8d2038754c38094521a335
Vortex Acherontic <vortex@z-ray.de>
2023-05-08 23:06:06 +0200

    Fix pypi script

--- pypi.sh
+++ pypi.sh
@@ -2,5 +2,5 @@
 rm -r ./dist/
 python3 -m build
 twine check dist/*
-python3 -m twine upload --repository pypi dist/*
+#python3 -m twine upload --repository pypi dist/*
 #python3 -m twine upload --repository testpypi dist/*
