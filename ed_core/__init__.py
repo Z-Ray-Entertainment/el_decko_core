41df28ecbf41350a20f694e1228efe198dc52289
Vortex Acherontic <vortex@z-ray.de>
2023-05-08 22:59:31 +0200

    Add DECK_TYPE to query_deck module

--- ed_core/__init__.py
+++ ed_core/__init__.py
@@ -4,7 +4,7 @@ from importlib.metadata import entry_points
 from ed_core import streamdeck
 from ed_core.streamdeck_config import load_config, apply_config
 
-VERSION = "2023.5.8"
+VERSION = "2023.5.8.1"
 BACKENDS = {}
 
 
--- ed_core/query_deck.py
+++ ed_core/query_deck.py
@@ -5,7 +5,8 @@ class QueryType(Enum):
     SERIAL = 0,
     KEY_COUNT = 1,
     KEY_LAYOUT = 2,
-    SUPPORTED_IMAGE_TYPE = 3
+    SUPPORTED_IMAGE_TYPE = 3,
+    DECK_TYPE = 4
 
 
 def query_deck(deck, prop: QueryType):
@@ -16,9 +17,11 @@ def query_deck(deck, prop: QueryType):
             prop_value = deck.get_serial_number()
         case QueryType.KEY_COUNT:
             prop_value = deck.key_count()
-        case QueryType.KEY_COUNT:
+        case QueryType.KEY_LAYOUT:
             prop_value = deck.key_layout()
         case QueryType.SUPPORTED_IMAGE_TYPE:
             prop_value = deck.key_image_format()
+        case QueryType.DECK_TYPE:
+            prop_value = deck.deck_type()
     deck.close()
     return prop_value
